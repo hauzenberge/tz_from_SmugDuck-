d7176c5363328f2668ade9c362b1f4df6d0640e1
Alexandr <shapovalik26@gmail.com>
2022-04-15 17:28:47 +0300

    first commit

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::create([
+        	'first_name' => 'Admin', 
+        	'last_name' => 'Adminskiy', 
+        	'phone' => '+380682092340', 
+        	'role_id' => 1, 
+        	'email' => 'admin@admin.com',
+        	'password' => bcrypt('password'),
+        ]);
+                
+        for ($i=0; $i <= rand(40, 100); $i++) { 
+            App\User::create([
+                'first_name' => 'Client '.$i, 
+                'last_name' => 'Adminskiy', 
+                'phone' => '+380682092340', 
+                'role_id' => 2, 
+                'email' => 'client'.$i.'@admin.com',
+                'password' => bcrypt('password'),
+            ]);
+        }
+    }
+}
